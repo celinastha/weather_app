fc097a26de148192a5affab7a94d9e0f75d05511
celinastha <celina.shrestha4@gmail.com>
2025-01-17 16:14:43 -0600

    Frontend add

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from './components/Search/Search'
+import CurrentWeather from './components/CurrentWeather/CurrentWeather'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Search />
+      <CurrentWeather />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/CurrentWeather/CurrentWeather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CurrentWeather.css'
+
+const CurrentWeather = () => {
+  return (
+    <div className='CurrentWeather'>
+        Current
+    </div>
+  )
+}
+
+export default CurrentWeather
