3811805fbc5dcbf14978d9205a479ff69e76ece9
celinastha <celina.shrestha4@gmail.com>
2025-02-10 21:22:11 -0600

    frontend edited

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,40 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Search.css'
 import { FiSearch } from "react-icons/fi"
+import { MdClear } from "react-icons/md"
+import { getFavoriteCities } from '../../utils/HandleApi'
 
-const Search = () => {
-
-    const [city, setCity] = useState("");
+const Search = ({ setCurrentCity, setCurrentMetricUnit, setFavCities, favCities }) => {
+    const [city, setCity] = useState("Austin");
     const [metricUnit, setMetricUnit] = useState("°F");
     const [searchInputValue, setSearchInputValue] = useState("");
     const [favCityValue, setFavCityValue] = useState("");
-    const [favBtnText, setFavBtnText] = useState("Fav City ❤️");
+    const [favBtnText, setFavBtnText] = useState("My Fav Cities ❤️");
     const [responsiveOptionOpen, setResponsiveOptionOpen] = useState(false);
+
+    useEffect(() => {
+        if(city){
+            setCurrentCity(city);
+        }
+        if(metricUnit){
+            setCurrentMetricUnit(metricUnit);
+        }
+        getFavoriteCities(setFavCities);
+    }, [city, setCurrentCity, metricUnit, setCurrentMetricUnit, setFavCities])
+
 
     const toggleResponsiveOption = () => {
         setResponsiveOptionOpen(!responsiveOptionOpen);
-        setFavBtnText(favBtnText === "Fav City ❤️" ? "Go Back" : "Fav City ❤️");
+        setFavBtnText(favBtnText === "My Fav Cities ❤️" ? "Go Back" : "My Fav Cities ❤️");
     };
 
+    const clearInputs = () => {
+        setSearchInputValue("");
+    };
 
     const handleChangeFavCity = (e) => {
         setFavCityValue(e.target.value);
         setCity(e.target.value);
         console.log("Current city is: ", e.target.value);
         setResponsiveOptionOpen(!responsiveOptionOpen);
-        setFavBtnText(favBtnText === "Fav City ❤️" ? "Go Back" : "Fav City ❤️");
+        setFavBtnText(favBtnText === "My Fav Cities ❤️" ? "Go Back" : "My Fav Cities ❤️");
     };
 
-
-    const handleSearchEnter = (e) => {
-        if(e.key === 'Enter'){
+    const handleSearch = (e) => {
+        e.preventDefault();
+        if(searchInputValue){
             setCity(searchInputValue);
             console.log("Current city is: ", searchInputValue);
             setFavCityValue("");
             setSearchInputValue("");
-            
+        } 
+        else{
+            return console.log("Need search city");
         }
     };
+
     const handleSearchChange = (e) => {
         setSearchInputValue(e.target.value);
     };
@@ -43,55 +61,68 @@ const Search = () => {
         setMetricUnit(e.target.value);
     };
 
+    const firstLetterCapital = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
 
     return (
         <div className='Search'>
-
-            <div className={`favMenuBtn ${responsiveOptionOpen ? 'close' : ''}`} onClick={toggleResponsiveOption}>
-                <span className='bar'></span>
-                <span className='bar'></span>
-                <span className='bar'></span>
-            </div>
-
-
-            <div className={`favMenu ${responsiveOptionOpen ? 'show' : ''}`}>
-                <h2>Fav Cities</h2>
-                    <ul className='favCitiesList'>
-                        <li><button className='favCity' value="City1" onClick={handleChangeFavCity}>City1</button></li>
-                        <li><button className='favCity' value="City2" onClick={handleChangeFavCity}>City2</button></li>
-                        <li><button className='favCity' value="City3" onClick={handleChangeFavCity}>City3</button></li>
-                    </ul>
-            </div>
+            <div className='searchContainer'>
+                <div className={`favMenuBtn ${responsiveOptionOpen ? 'close' : ''}`} onClick={toggleResponsiveOption}>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                </div>
 
 
-            <button className='favCityButton' onClick={toggleResponsiveOption}>
-                {favBtnText}
-            </button>
-            
+                <div className={`favMenu ${responsiveOptionOpen ? 'show' : ''}`}>
+                    <div className='title_description'>
+                        <h2>Fav Cities</h2>
+                        <p className='favDescription'>Click on the heart next to cities to make it ur fav city!</p>
+                    </div>
+                        <ul className='favCitiesList'>
+                            {favCities.map((favCity) => (
+                                <li key={favCity.id}>
+                                    <button className='favCity' value={favCity.city} onClick={handleChangeFavCity}>
+                                        {firstLetterCapital(favCity.city)}
+                                    </button>
+                                </li>))}
+                        </ul>
+                </div>
 
-            <div className='searchInputContainer'>
-                <input className='searchInput' id='searchInput' type='text' value={searchInputValue} placeholder="" onChange={handleSearchChange} onKeyDown={handleSearchEnter} />
-                <label className='searchLabel' htmlFor="searchInput"> Search any city </label>
-                <FiSearch className='searchIcon'/>
-            </div>
+
+                <button className='favCityButton' onClick={toggleResponsiveOption}>
+                    {favBtnText}
+                </button>
                 
 
-            <select className='metricUnitSelect' value={metricUnit} onChange={handleMetricChange}>
-                <option value="°F">°F</option>
-                <option value="°C">°C</option>
-            </select>
+                <form className='searchInputContainer' onSubmit={handleSearch}>
+                    <input className='searchInput' id='searchInput' type='text' value={searchInputValue} placeholder="" onChange={handleSearchChange}/>
+                    <label className='searchLabel' htmlFor="searchInput"> Search any city </label>
+                    <button className={`clearInput ${searchInputValue ? 'show' : ''}`} type="button" onClick={clearInputs}><MdClear className='clear'/></button>
+                    <button className='searchIconContainer' type="submit"><FiSearch className='searchIcon'/></button>
+                </form>
+                    
+
+                <select className='metricUnitSelect' value={metricUnit} onChange={handleMetricChange}>
+                    <option value="°F">°F</option>
+                    <option value="°C">°C</option>
+                </select>
 
 
-            
+                
 
-            <div className='debugHelpers'>
-                <p>Current city is: {city}</p>
-                <p>Fav City Current value is: {favCityValue}</p>
-                <p>Input value Current is: {searchInputValue}</p>
-                <p>Metric unit is: {metricUnit}</p>
+                <div className='debugHelpers'>
+                    <p>Current city is: {city}</p>
+                    <p>Fav City Current value is: {favCityValue}</p>
+                    <p>Input value Current is: {searchInputValue}</p>
+                    <p>Metric unit is: {metricUnit}</p>
+                </div>
             </div>
         </div>
     )
 }
+
 
 export default Search
---
+++ frontend/src/utils/HandleApi.js
@@ -0,0 +1,97 @@
+import { toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+const baseUrl = "http://localhost:8080";
+
+const getCurrentWeather = async (city, metricUnit, setWeather, setError) => {
+    var unitUsed = "imperial";
+    if(metricUnit === "°F"){
+        unitUsed = "imperial";
+    } else if(metricUnit === "°C"){
+        unitUsed = "metric";
+    }
+    try {
+        const response = await fetch(`${baseUrl}/search/${city}?unit=${encodeURIComponent(unitUsed)}`);
+        if(response){
+            setError('');
+        }
+        const weatherData = await response.json();
+        console.log("Current city weather - ", weatherData);
+        setWeather({
+            city: weatherData.name,
+            iconCode: weatherData.weather[0].icon,
+            temperature: Math.floor(weatherData.main.temp),
+            description: weatherData.weather[0].main,
+            feelsLike: Math.floor(weatherData.main.feels_like),
+            windSpeed: weatherData.wind.speed,
+            humidity: weatherData.main.humidity,
+            visibility: weatherData.visibility,
+            pressure: weatherData.main.pressure,
+            tempMax: Math.floor(weatherData.main.temp_max),
+            tempMin: Math.floor(weatherData.main.temp_min),
+            clouds: weatherData.clouds.all
+        });
+    } catch(err){
+        console.log(err);
+        setError(`Can't find Weather info for ${city}`);
+    }
+}
+
+
+const getFavoriteCities = async (setFavCities) => {
+    try {
+        const response = await fetch(`${baseUrl}/getFavCities`);
+        const favData = await response.json();
+        console.log("fav Data: ", favData);
+        setFavCities(favData);
+    } catch(err){
+        console.log(err);
+    }
+}
+
+const savFavCities = async (currentCity, setFavCities) => {
+    try{
+        const response = await fetch(`${baseUrl}/saveFavCity`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({city: `${currentCity}`})
+        });
+        const responseTxt = await response.text();
+        console.log('Response: ', responseTxt);
+        toast.success(responseTxt);
+        getFavoriteCities(setFavCities);
+    }
+    catch(err){
+        console.error('Error: ', err);
+    }
+}
+
+const removeFavCities = async (currentCity, setFavCities) => {
+    try{
+        const response = await fetch(`${baseUrl}/deleteFavCity`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({city: `${currentCity}`})
+        });
+        const responseTxt = await response.text();
+        console.log('Response: ', responseTxt);
+        toast.warn(responseTxt);
+        getFavoriteCities(setFavCities);
+    }
+    catch(err){
+        console.error('Error: ', err);
+    }
+}
+
+export {getCurrentWeather, getFavoriteCities, savFavCities, removeFavCities}
+
+/*axios
+    .get(`${baseUrl}/search/${city}`)
+    .then(({data}) => {
+        console.log("data -> ", data);
+    })
+    .catch((err) => console.log("Error: ", err))*/
