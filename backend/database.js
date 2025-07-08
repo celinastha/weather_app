0f45b0f804364dbef73891510ec42e8f5e0b364c
Celina Shrestha <celina.shrestha4@gmail.com>
2025-07-08 16:59:18 -0500

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 require('dotenv').config();
 
 const database = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: import.meta.env.DB_HOST,
+    user: import.meta.env.DB_USER,
+    password: import.meta.env.DB_PASS,
+    database: import.meta.env.DB_NAME
 });
 
 database.connect((err) => {
